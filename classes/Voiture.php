47fcfc6a4ac82d8121611a02adb38a89095f5f7d
Frédéric NOËL <contact@fredericnoel.com>
2020-01-29 10:23:00 +0100

    Ecriture classe Voiture

---
+++ classes/Voiture.php
@@ -0,0 +1,14 @@
+<?php
+
+class Voiture
+{
+  public $nbrRoues;
+  public $nbrPlaces;
+  public $masse;
+  public $vitesse = 0;
+
+  public function afficherMessageDebile()
+  {
+    echo "Je roule en caisse";
+  }
+}
