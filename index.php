3c106860f1571dc1bd17e930bb89426d696b5b0d
Frédéric NOËL <contact@fredericnoel.com>
2020-01-29 11:50:04 +0100

    Essais divers et variés du mercredi matin.Bisous.

--- index.php
+++ index.php
@@ -2,8 +2,14 @@
 
 require_once './classes/Voiture.php';
 
-$voiture1 = new Voiture();
+$voiture1 = new Voiture(1000, 'rouge');
+$voiture2 = new Voiture(2000, 'vert pomme');
+$voiture1->vitesse = 30;
+$voiture2->vitesse = 40;
 
-$voiture1->masse = 1000;
+echo $voiture1->getNbrPlaces();
 
-$voiture1->afficherMessageDebile();
+echo $voiture1->calculerEnergieCinetique();
+echo "<br />";
+$voiture1->vitesse = 10;
+echo $voiture1->calculerEnergieCinetique();
